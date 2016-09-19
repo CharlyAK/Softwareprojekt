a7f60992e1e92c57fc8d4db83efd7b9bcf020da3
GinaSeckendorf <gina.seckendorf@st.ovgu.de>
2016-09-19 15:04:19 +0200

    add audioTest

--- app/src/main/java/soprowerwolf/Activities/LetsPlayActivity.java
+++ app/src/main/java/soprowerwolf/Activities/LetsPlayActivity.java
@@ -1,6 +1,8 @@
 package soprowerwolf.Activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,14 +11,31 @@ import soprowerwolf.R;
 
 public class LetsPlayActivity extends AppCompatActivity {
 
+    MediaPlayer audio;
+    CountDownTimer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lets_play);
+
+        audio = MediaPlayer.create(this, R.raw.pocahontas);
+
+        timer = new CountDownTimer(audio.getDuration(), 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                audio.start();
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(LetsPlayActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        }.start();
     }
 
     public void letsPlay(View view){
-        Intent intent = new Intent(LetsPlayActivity.this, GameActivity.class);
-        startActivity(intent);
+        timer.onFinish();
     }
 }
